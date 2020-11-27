3735ea91bed26a23da20b9da03079b48f04cc5a3
Roman <melnikov.roman.lang@gmail.com>
2020-11-27 16:07:03 +0600

    add and delete elements of array

--- 02-Arrays.js
+++ 02-Arrays.js
@@ -48,3 +48,32 @@ console.log(maniacs[1]);
 console.log(maniacs[2]);
     console.log(maniacs.length);
     console.log(maniacs[maniacs.length - 1]);
+
+/* ----- Add and Delete elements in the array ----- */
+
+	/*
+	.push() - добавляет элемент в конец массива
+	.unshift() - добавляет элемент в начало массива и двигает остальные элементы вперед
+	*/
+	const fruits = [];
+	fruits.push('apple');
+	fruits.push('orange');
+	fruits.push('lemon');
+		console.log(fruits);
+		console.log(fruits.length);
+	fruits.unshift('banana');
+	fruits.unshift('kiwi');
+		console.log(fruits);
+		console.log(fruits.length);
+	/*
+	.pop() - удаляет последний элемент из массива и сохраняет значение в новой переменной
+	.shift() - удаляет первый элемент из массива и сохраняет значение в новой переменной
+	*/
+	const lastFruit = fruits.pop();
+		console.log(lastFruit);
+		console.log(fruits);
+	fruits.unshift(lastFruit);
+		console.log(fruits);
+	const firstFruit = fruits.shift();
+		console.log(firstFruit);
+		console.log(fruits);
